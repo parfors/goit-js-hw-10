42a4adb6e7c6fbe48dc3b30bb732e586d56000d7
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-06-10 15:34:26 +0300

    start hw 10

---
+++ src/fetchCountries.js
@@ -0,0 +1,9 @@
+
+function fetchCountries(name) {
+    const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data));         
+}
+export { fetchCountries }
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import './css/styles.css';
+import debounce from  'lodash.debounce'
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('#search-box'),
+}
+
+refs.input.addEventListener('input', debounce(inputHandler, 300));
+
+function inputHandler(e) {
+    const {  value: countryName } = e.target; 
+    if (countryName) {
+        fetchCountries(countryName.trim());
+    }
+    else {}
+}
+
+
+// console.log(fetchCountries('ukraine'));
+
+
+// const url = 'https://restcountries.com/v3.1/name/peru'
+
+// fetch(url).then(response => response.json()).
+//     then(data => console.log(data[0].name.official));
+
+
+
+
+// const href = location.href;
+// const url1 = new URL(href);
+// console.log(url1);
